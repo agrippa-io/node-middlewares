1e67d8b0af92e577808a650eeea4e47ab5b446f1
Matt Valli <matthew.valli@gmail.com>
2021-10-26 22:25:13 -0700

    Removed type module from package.json; Upgraded to v1.0.2

---
+++ dist/ServerConfiguration/catchAllErrors.d.ts
@@ -0,0 +1,3 @@
+import ErrorAPI from '@agrippa-io/node-errors/src/ErrorAPI';
+declare const _default: (error: ErrorAPI, request: any, response: any, next: any) => Promise<void>;
+export default _default;
